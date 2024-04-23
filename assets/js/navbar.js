7744c90e000dc1a4afd2e13236c9f1eba3563661
Jordan DuBois <39501741+reflecting-walls@users.noreply.github.com>
2024-04-23 11:13:22 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,7 @@
+function toggleMenu() {
+    var navbarToggle = document.querySelector('.navbar-toggle');
+    var navLinks = document.querySelector('.nav-links');
 
+    navbarToggle.classList.toggle('change');
+    navLinks.classList.toggle('show');
+}
