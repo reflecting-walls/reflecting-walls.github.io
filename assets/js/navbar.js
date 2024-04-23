bc5c41fb552d499eef327d94e811dac4ef97fac4
Jordan DuBois <39501741+reflecting-walls@users.noreply.github.com>
2024-04-23 12:43:58 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
-// Function to toggle visibility of links for hamburger menu
-function toggleMenu() {
-    var links = document.querySelector('.links');
-    links.style.display = links.style.display === 'none' ? 'flex' : 'none';
-}
+    // Function to toggle visibility of links for hamburger menu
+    function toggleMenu() {
+        var links = document.querySelector('.links');
+        links.style.display = links.style.display === 'none' ? 'flex' : 'none';
+    }
